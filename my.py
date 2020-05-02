63e5812995827aac2cef037bb3bfeb4f24250faa
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-05-02 11:54:25 +0200

    working on this

--- my.py
+++ my.py
@@ -17,18 +17,34 @@ def readSNPfile(vcfFile):
 		#              POS 
 		snpdict[tuple((l[1]))] = elem
 
-	print(snpdict)
+	return snpdict
+
+def getSNPs(varientRange, sbjctlist):
+	sbjctdict = readSNPfile(sbjctlist)
+	ctrldict = readFile(varientRange)
+
+
+	# This returns dict keys that are in sbjctdict but not in ctrldict
+	print(ctrldict)
+	#print(sbjctdict)
+	snps = set(sbjctdict).difference(set(ctrldict))
+	return [sbjctdict[snp] for snp in snps]
+
 def readFile(fname):
 		value1=numpy.loadtxt(fname, delimiter="\t",usecols=[1])
 		value2=numpy.loadtxt(fname, delimiter="\t",usecols=[2])
 		i=0
-		while i < len(value1):
-			print(numpy.arange(value1[i],value2[i]))
-			i+=1
-			
+		#while i < len(value1):
+		return numpy.arange(value1[i],value2[i])
+		#	i+=1			
 if __name__ == '__main__':
 	fname = sys.argv[1]
 	vcfFile = sys.argv[2]
-	readFile(fname)
-	readSNPfile(vcfFile)
+	result=getSNPs(fname,vcfFile)
+
+	with open('result.txt', 'w') as out:
+		for snp in result:
+			out.write(snp)
+
+	print('number of SNPs found:', len(result))
 	
