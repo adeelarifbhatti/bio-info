d35ea80c9b757911d2a9a6e5f032db16e3f33f6a
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-05-03 05:32:18 +0200

    Reading the file and generating the sequence of the variants

--- write.py
+++ write.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python2
 from __future__ import with_statement, print_function
 import sys
-def readFile(fname):
-	for x in range(int(fname)):
-		F = open("devops.txt", "a")
-		F.write(str(x))
-		F.write('\n')
-  		#print(x)
-
+def readFile():
+	file = open("intervals.bed","r")
+	for line in file:
+  		fields = line.split("\t")
+  		chromosome = fields[0]
+  		start = fields[1]
+  		stop = fields[2]
+  		print(chromosome +'\t'+ start+ '\t'+ stop)
+  		F = open("devops.txt", "a")
+		for x in range(int(start), int(stop)):
+			F = open("devops.txt", "a")
+			F.write(chromosome+'\t'+str(x))
+			F.write('\n')
 if __name__ == '__main__':
-	fname = sys.argv[1]
-	readFile(fname)
+	readFile()
