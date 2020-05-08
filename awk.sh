a93dfcb671530cf98ab213f43462816a9c021540
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-05-08 17:20:47 +0200

    awk works

--- awk.sh
+++ awk.sh
@@ -1,5 +1,4 @@
 while IFS=$'\t' read -r column1 column2 column3 ; do
-
 awk -v col3="${column3}" -v col2="$column2" '{if ($2 >= col2 && $2<=col3) print $2}' subject.variants.vcf >> Arif.txt
 awk -v col3="${column3}" -v col2="$column2" '{if ($2 >= col2 && $2<=col3) print $2}' control.variants.vcf >> Arif.txt
 done < "intervals.bed"
