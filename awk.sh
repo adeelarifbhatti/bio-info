b1fd86c962ac8a608715a80f126e61ae2703f7b4
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-05-12 18:13:54 +0200

    changes in awk

--- awk.sh
+++ awk.sh
@@ -1,6 +1,15 @@
+FILE=count.txt
+if test -f "$FILE"; then
+    echo "$FILE exist"
+    echo "deleting $FILE"
+    rm ./count.txt
+fi
+cat control.variants.vcf > sample.data
+cat subject.variants.vcf >> sample.data
+
+
 while IFS=$'\t' read -r column1 column2 column3 ; do
-awk -v col3="${column3}" -v col2="$column2" '{if ($2 >= col2 && $2<=col3) print $2}' subject.variants.vcf >> Arif.txt
-awk -v col3="${column3}" -v col2="$column2" '{if ($2 >= col2 && $2<=col3) print $2}' control.variants.vcf >> Arif.txt
+awk -v col3="${column3}" -v col2="$column2" '{if ($2 >= col2 && $2<=col3) print $2}' sample.data >> count.txt
 done < "intervals.bed"
 
-wc -l Arif.txt
+wc -l count.txt
