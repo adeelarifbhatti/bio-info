457754caef6dcee1c3fae82d94484b9b7a9f91b7
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-05-12 20:13:34 +0200

    New approach

---
+++ question2-2a.py
@@ -0,0 +1,60 @@
+import numpy as numpy 
+import thread
+#from thread import start_new_thread
+import sys
+def makeVariantArray(vcffile):
+	snplist = []
+	varientlist=[]
+	with open(vcffile, 'r') as f:
+		for line in f:
+			if line.startswith('#'):
+				continue
+			snplist.append(line)
+
+	for elem in snplist:
+		l = elem.split('\t')
+		#       POS   ALT
+		varientlist.append(l[1])
+	#print("From varientmethod")
+	varientArray = numpy.asarray(varientlist) 
+	#print(varientArray)
+	return varientArray
+def readFile(fname,i,j):	
+	rangelist = []
+	result2=0
+	b = []
+	with open(fname, 'r') as f:
+		for line in f:
+			if line.startswith('#'):
+				continue
+			rangelist.append(line)
+			rangedict = {}
+		for x in range(i,j):
+			fields = rangelist[x].split('\t')
+			print(x)
+			chromosome = fields[0]
+  			start = fields[1]
+  			stop = fields[2]
+			a=[numpy.arange(int(start),int(stop))]
+			##b=a
+			result=numpy.intersect1d(vcffiles,a)
+			print('Number of Variants in Interval' ,fields[1] ,'and',fields[2],' is ', len(result))
+			result2 += len(result)
+
+		print('"""""""""""Total Number of Variants are:"""""""""""""', result2)
+		return b
+if __name__ == '__main__':
+	fname = sys.argv[1]
+	vcffile= sys.argv[2]
+	vcffile2=sys.argv[3]
+	vcffiles=numpy.append(makeVariantArray(vcffile),makeVariantArray(vcffile2))
+
+	#thread.start_new_thread(readFile, (fname,))
+	readFile(fname,1,4)
+	readFile(fname,5,8)
+
+	print('"""""""""""Total Number of Variants are:""""""""""""')
+	
+	
+
+	
